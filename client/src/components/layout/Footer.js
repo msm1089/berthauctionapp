cc9729f92768047238bb60be907ad839fbed722e
sam-w-thomas <36865298+sam-w-thomas@users.noreply.github.com>
2020-07-09 10:25:18 +0100

    dasdadsasd

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { red } from '@material-ui/core/colors';
 
 const Footer = () => {
-<<<<<<< HEAD
     
     return(
         <div>
@@ -16,32 +15,15 @@ const Footer = () => {
                 position: 'sticky', 
                 bottom: '0',
                 width: '100%', 
-                height: '20px',
+                height: '20px'
             }}> 
             
             Find out more about us:
             <a href="https://www.portsmouth-port.co.uk/"> here</a>
-=======
-  return (
-    <div>
-      <body
-        style={{
-          backgroundColor: '#343a40',
-          color: 'white',
-          fontSize: 'small',
-          textAlign: 'center',
-          position: 'absolute',
-          bottom: '0',
-          width: '100%',
-          height: '20px',
-        }}
-      >
-        Find out more about us:
-        <a href="https://www.portsmouth-port.co.uk/"> here</a>
-      </body>
-    </div>
-  );
-};
->>>>>>> 6bd6464beea0c4ba0333bdcaed13bb11a371a54f
+
+        </body>
+        </div>      
+    )
+}
 
 export default Footer;
